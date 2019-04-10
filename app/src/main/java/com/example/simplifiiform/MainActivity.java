91585919a890c1022653a4cdd813947b7e79f97f
meditat <shivam.singh.calif@gmail.com>
2019-04-10 14:55:23 +0530

    finishing some tasks

--- app/src/main/java/com/example/simplifiiform/MainActivity.java
+++ app/src/main/java/com/example/simplifiiform/MainActivity.java
@@ -1,31 +1,42 @@
 package com.example.simplifiiform;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 
 import androidx.appcompat.app.AppCompatActivity;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Html;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.ramotion.fluidslider.FluidSlider;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.zhouyou.view.seekbar.SignSeekBar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -35,8 +46,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Objects;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -47,7 +57,13 @@ public class MainActivity extends AppCompatActivity {
     static List<Model> models;
     LinearLayout linearLayout;
 
-    int TEXT_ID = 5;
+    int TITLE_ID = 6;
+    int MIN_ID = 7;
+    int MAX_ID = 8;
+    int SEEK_ID = 9;
+
+    TextInputEditText editText;
+    SignSeekBar signSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                URL apiUrl = new URL("https://ca.platform.simplifii.xyz/api/v1/static/assignment2");
+                URL apiUrl = new URL("https://api.myjson.com/bins/135kt4");
                 HttpsURLConnection connection = (HttpsURLConnection) apiUrl.openConnection();
                 InputStream inputStream = connection.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -145,87 +161,187 @@ public class MainActivity extends AppCompatActivity {
                     if (model.getType().equals("range")) {
                         inputTypeRange(model);
                     }
-                    if (model.getType().equals("input")){
-                        final EditText editText = new EditText(context);
-                        editText.setHint(model.getLabel());
-                        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                        lp.setMargins(16, 16, 16, 16);
-                        editText.setLayoutParams(lp);
-                        linearLayout.addView(editText);
-
-                        editText.addTextChangedListener(new TextWatcher() {
-                            @Override
-                            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                                if (!isValidEmail(s.toString())){
-                                    editText.setError("Invalid email");
-                                }
-                            }
-
-                            @Override
-                            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                                if (!isValidEmail(s.toString())){
-                                    editText.setError("Invalid email");
-                                }
-                            }
-
-                            @Override
-                            public void afterTextChanged(Editable s) {
-                                if (!isValidEmail(s.toString())){
-                                    editText.setError("Invalid email");
-                                }
-                            }
-                        });
+                    if (model.getType().equals("input")) {
+                        makeInputField(model);
                     }
+                    if (model.getType().equals("button")) {
+                        makeButton(model);
+                    }
+
                 }
 
             }
         }
 
-        private void inputTypeRange(Model model) {
-            TextView title = new TextView(context);
-            title.setText(model.getLabel());
-            title.setTextSize(18);
-            title.setId(TEXT_ID);
-            LinearLayout.LayoutParams trp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            trp.setMargins(16,2,0,4);
-            title.setLayoutParams(trp);
+        private void makeButton(final Model model) {
+            Button button = new Button(context);
+            button.setText(model.getLabel());
+            button.setPadding(16, 0, 16, 0);
+            linearLayout.setPadding(16, 0, 16, 0);
+            LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            button.setLayoutParams(buttonParams);
+
+            linearLayout.addView(button);
 
 
-            final FluidSlider fluidSlider = new FluidSlider(context);
-            fluidSlider.setTextSize(40);
-            final int min = model.getMin();
-
-            final int max = model.getMax();
-            int initialText = (int) (0.5 * (max - min));
-            fluidSlider.setBubbleText(String.valueOf(initialText));
-            fluidSlider.setPositionListener(new Function1<Float, Unit>() {
+            button.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public Unit invoke(Float aFloat) {
-                    float pos = aFloat;
-                    int total = max - min;
-                    int text = (int) (min + total * pos);
-                    fluidSlider.setBubbleText(String.valueOf(text));
-                    return null;
+                public void onClick(View v) {
+                    if (editText.getText().toString().isEmpty()) {
+                        Log.d(TAG, "onClick: Somethting bad");
+                        Snackbar snackbar = Snackbar.make(linearLayout,  Html.fromHtml("<font color=\"#FFFFFF\">Fields can't be empty</font>"), Snackbar.LENGTH_LONG)
+                                .setAction("Action", null);
+                        View sbView = snackbar.getView();
+                        sbView.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimaryDark));
+                        snackbar.show();
+                    } else {
+                        postData(model);
+                    }
                 }
             });
-            fluidSlider.setColorBar(R.color.colorPrimary);
-            fluidSlider.setStartText(String.valueOf(model.getMin()));
-            fluidSlider.setEndText(String.valueOf(model.getMax()));
+        }
 
-            LinearLayout.LayoutParams flp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            flp.setMargins(16, 2, 16, 2);
-            fluidSlider.setLayoutParams(flp);
+        void postData(Model model) {
 
-            linearLayout.addView(title);
-            linearLayout.addView(fluidSlider);
+            try {
+                AsyncHttpClient client = new AsyncHttpClient();
+                // Http Request Params Object
+                RequestParams params = new RequestParams();
+                for(int i = 0;i<models.size();i++){
+
+                    Model model1 = models.get(i);
+                    if (model1.getType().equals("range")){
+                        params.put(model1.getLabel(), String.valueOf(signSeekBar.getProgress()));
+
+                    }if (model1.getType().equals("input")){
+                        params.put(model1.getLabel(), editText.getText().toString());
+                    }
+                }
+
+
+                client.post(model.getApiUri(), params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(String response) {
+                        showDialog();
+                        Log.i("Response", "Response SP Status. " + response);
+
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        super.onFailure(throwable);
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        private void makeInputField(final Model model) {
+            editText = new TextInputEditText(context);
+            LinearLayout.LayoutParams editTextParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT);
+
+            TextInputLayout textInputLayout = new TextInputLayout(context);
+            LinearLayout.LayoutParams textInputLayoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            textInputLayout.setLayoutParams(textInputLayoutParams);
+            textInputLayout.addView(editText, editTextParams);
+            textInputLayout.setHint(model.getLabel());
+
+
+            editTextParams.setMargins(16, 16, 16, 16);
+            linearLayout.addView(textInputLayout);
+
+            editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if (!isValidEmail(s)) {
+                        editText.setError(model.getValidationMsg());
+                    }
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
         }
 
 
-        public final  boolean isValidEmail(CharSequence target) {
+        private void inputTypeRange(Model model) {
+
+            //dynamic layout
+            RelativeLayout relativeLayout = new RelativeLayout(context);
+            relativeLayout.setPadding(16, 16, 16, 0);
+
+            //layout params for title and seekbar
+            RelativeLayout.LayoutParams titlePars = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            RelativeLayout.LayoutParams seekBar = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            //seekbar should be below title as one unit
+            titlePars.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            seekBar.addRule(RelativeLayout.BELOW, TITLE_ID);
+
+            //title text
+            TextView title = new TextView(context);
+            title.setText(model.getLabel());
+            title.setTextSize(18);
+            title.setPadding(0, 0, 0, 32);
+            title.setId(TITLE_ID);
+            title.setLayoutParams(titlePars);
+
+            //seekbar for range
+            signSeekBar = new SignSeekBar(context);
+            signSeekBar.getConfigBuilder()
+                    .min(model.getMin())
+                    .max(model.getMax())
+                    .sectionCount(1)
+                    .sectionTextSize(16)
+                    .thumbTextSize(12)
+                    .showSectionText()
+                    .showThumbText()
+                    .signTextSize(18)
+                    .sectionTextPosition(SignSeekBar.TextPosition.SIDES)
+                    .build();
+            signSeekBar.setLayoutParams(seekBar);
+            signSeekBar.setPadding(16, 0, 16, 0);
+            signSeekBar.setId(SEEK_ID);
+
+            //add view to layout
+            relativeLayout.addView(title);
+            relativeLayout.addView(signSeekBar);
+
+            linearLayout.addView(relativeLayout);
+
+
+        }
+
+
+        public final boolean isValidEmail(CharSequence target) {
             if (target == null)
                 return false;
 
             return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+
+        private void showDialog() {
+            Dialog dialog = new Dialog(context);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.completion_dialog);
+            dialog.setCanceledOnTouchOutside(true);
+            dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+            dialog.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
         }
     }
 }
